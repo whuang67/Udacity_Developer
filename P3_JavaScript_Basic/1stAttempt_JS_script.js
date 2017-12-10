c1914dc1daa64fad863b10a6dc1e4522357f3a59
Wenke Huang <whuang67@illinois.edu>
2017-12-09 23:13:42 -0600

    Add files via upload

---
+++ P3_JavaScript_Basic/1stAttempt_JS_script.js
@@ -0,0 +1,4 @@
+// This is an exercise that mainly focuses on the usages of prompt, alert and console.log
+var userName = prompt("What is your name?");
+alert("Nice to meet you, " + userName);
+console.log("Also great to meet you, " + userName);
